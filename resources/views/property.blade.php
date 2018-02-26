a53e676f764f507598fe063b262d384aaf4df1fb
Mark Powalisz <markpowalisz@Fenix-Macbook-Pro.local>
2018-02-26 13:10:25 -0500

    New Laravel

---
+++ resources/views/property.blade.php
@@ -0,0 +1,7 @@
+@extends ("layouts.app") 
+@section('content')
+<div class="container">
+    <h3>Properties</h3>
+    {{--  @foreach ( Properties as Properties ) @endforeach  --}}
+</div>
+@endsection
