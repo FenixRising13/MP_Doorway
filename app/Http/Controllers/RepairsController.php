667bc7d4d9e4ef77c1269b728de0fd9ebb3109eb
Mark Powalisz <markpowalisz@Fenix-Macbook-Pro.local>
2018-03-02 14:44:57 -0500

    Test

--- app/Http/Controllers/RepairsController.php
+++ app/Http/Controllers/RepairsController.php
@@ -17,8 +17,8 @@ class RepairsController extends Controller
     public function index()
     {
         $repairs = Repair::all()->where("completed"==1);
-        $incomplete = Repair::all()->where("completed"==0);
-        return view('repairs.index',compact('repairs','incomplete'));
+        // $incomplete = Repair::all()->where("completed"==0);
+        return view('repairs.index',compact('repairs'));
     }
 
     /**
