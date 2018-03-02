8fce432e70bf23e0007d11503e66359fce66076c
Mark Powalisz <markpowalisz@Fenix-Macbook-Pro.local>
2018-03-02 14:01:26 -0500

    Made change to Repairs@index

--- app/Http/Controllers/RepairsController.php
+++ app/Http/Controllers/RepairsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Facades\DB;
 
 use App\Repair;
 
@@ -16,7 +16,8 @@ class RepairsController extends Controller
      */
     public function index()
     {
-        $repairs = Repair::all();
+        
+        $repairs = Repair::all()->where("completed",1);
         return view('repairs.index',compact('repairs'));
     }
 
