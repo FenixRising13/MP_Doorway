d99f17b244ba7abf63d2288d4d7466f7d275f848
Mark Powalisz <markpowalisz@Fenix-Macbook-Pro.local>
2018-03-02 11:22:27 -0500

    changed css from asset to secure_asset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,6 +70,6 @@
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ secure_asset('js/app.js') }}"></script>
 </body>
 </html>
