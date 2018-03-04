2979a3555ef4ab421c297b9f9ebd2f4d9b5fea08
Mark Powalisz <markpowalisz@Fenix-Macbook-Pro.local>
2018-03-04 17:36:18 -0500

    Playing with create.blade for properties

--- resources/views/properties/create.blade.php
+++ resources/views/properties/create.blade.php
@@ -1,55 +1,13 @@
 @extends('layouts/app') 
 @section('content')
     <div class="container">
-        <div class="preloader-wrapper small active">
-          <!-- spinner-blue -->
-          <div class="spinner-layer spinner-blue">
-            <div class="circle-clipper left">
-              <div class="circle"></div>
-            </div>
-            <div class="gap-patch">
-              <div class="circle"></div>
-            </div>
-            <div class="circle-clipper right">
-              <div class="circle"></div>
-            </div>
-          </div>
-          <!-- spinner-red -->
-          <div class="spinner-layer spinner-red">
-            <div class="circle-clipper left">
-              <div class="circle"></div>
-            </div>
-            <div class="gap-patch">
-              <div class="circle"></div>
-            </div>
-            <div class="circle-clipper right">
-              <div class="circle"></div>
-            </div>
-          </div>
-          <!-- spinner-yellow -->
-          <div class="spinner-layer spinner-yellow">
-            <div class="circle-clipper left">
-              <div class="circle"></div>
-            </div>
-            <div class="gap-patch">
-              <div class="circle"></div>
-            </div>
-            <div class="circle-clipper right">
-              <div class="circle"></div>
-            </div>
-          </div>
-          <!-- spinner-greem -->
-          <div class="spinner-layer spinner-green">
-            <div class="circle-clipper left">
-              <div class="circle"></div>
-            </div>
-            <div class="gap-patch">
-              <div class="circle"></div>
-            </div>
-            <div class="circle-clipper right">
-              <div class="circle"></div>
-            </div>
-          </div>
+       This is a badge
+        <span class="badge">8</span>
+
+        <div class="chip">
+            <img src="" alt=""/>
+            Chip
         </div>
+
     </div>
 @endsection
