26dfe15aa4b7514656f89f24d450f9df6255c1a2
Mark Powalisz <markpowalisz@Fenix-Macbook-Pro.local>
2018-03-05 11:00:55 -0500

    Updating late rent form

--- resources/views/laterent.blade.php
+++ resources/views/laterent.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-{{$Date()}}
-{{$property->tenant}}<br>
-{{$property->addr}}<br>
-{{$property->addr2}}<br>
-{{$property->city}}{{$property->state}}{{$property->zip}}
+{{$properties->tenant}}<br>
+{{$properties->addr}}<br>
+{{$properties->addr2}}<br>
+{{$properties->city}}{{$properties->state}}{{$properties->zip}}
 <br>
 <p>Dear Tenants,</p>
 <br>
