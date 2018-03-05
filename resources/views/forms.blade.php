06e2959c833ebf0e9eb5d061fb8292add49d7bb3
Mark Powalisz <markpowalisz@Fenix-Macbook-Pro.local>
2018-03-05 11:15:57 -0500

    Updating forms

--- resources/views/forms.blade.php
+++ resources/views/forms.blade.php
@@ -2,10 +2,10 @@
 @section ('content')
 <div class="container">
   <ul class="collection">
-    <li class="collection-item" onclick="location.href = '#';">Three-Day Notice (Pre-Eviction)</li>
-    <li class="collection-item">Change of Address</li>
-    <li class="collection-item" onclick="location.href = '/laterent';">Late Rent Notice</li>
-    <li class="collection-item">Lease Renewal Notice</li>
+    <li class="collection-item"><a href="/threedaynotice">Three-Day Notice (Pre-Eviction)</a></li>
+    <li class="collection-item"><a href="/changeaddress">Change of Address</a></li>
+    <li class="collection-item"><a href="/laterent">Late Rent Notice</a></li>
+    <li class="collection-item"><a href="/leaserenewal">Lease Renewal Notice</a></li>
   </ul>
 </div>
 @endsection
