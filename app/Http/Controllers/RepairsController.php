f9d6bfb73384b21a9ec2dcc935938648a9d35ac3
Mark Powalisz <markpowalisz@Fenix-Macbook-Pro.local>
2018-03-05 12:39:57 -0500

    Changed repairs@index

--- app/Http/Controllers/RepairsController.php
+++ app/Http/Controllers/RepairsController.php
@@ -20,8 +20,8 @@ class RepairsController extends Controller
     
     {
         // Attempt to access other table
-        $CurrentUser = User::id(1)->name;
-        $Property = Property::id(1)->addr;
+        $CurrentUser = User::all();
+        $Property = Property::all();
 
         $repairs = Repair::all()->where("completed",true);
         $incomplete = Repair::all()->where("completed",false);
