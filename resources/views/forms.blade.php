4b40565f9a545a4cbb0df64a9f0b2bc3ed7fb11f
Mark Powalisz <markpowalisz@Fenix-Macbook-Pro.local>
2018-03-05 00:12:05 -0500

    Href Test on forms

--- resources/views/forms.blade.php
+++ resources/views/forms.blade.php
@@ -2,7 +2,7 @@
 @section ('content')
 <div class="container">
   <ul class="collection">
-    <li class="collection-item">Three-Day Notice (Pre-Eviction)</li>
+    <li class="collection-item">Three-Day Notice (Pre-Eviction) <a href="/" ></a></li>
     <li class="collection-item">Change of Address</li>
     <li class="collection-item">Late Rent Notice</li>
     <li class="collection-item">Lease Renewal Notice</li>
