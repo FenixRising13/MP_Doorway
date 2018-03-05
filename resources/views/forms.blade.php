f1bcf3fc2dc6750f8b2d4ee3f34a4f7f25746c00
Mark Powalisz <markpowalisz@Fenix-Macbook-Pro.local>
2018-03-05 11:53:44 -0500

    Updated forms links to class link

--- resources/views/forms.blade.php
+++ resources/views/forms.blade.php
@@ -2,10 +2,10 @@
 @section ('content')
 <div class="container">
   <ul class="collection">
-    <li class="collection-item"><a href="/threedaynotice">Three-Day Notice (Pre-Eviction)</a></li>
-    <li class="collection-item"><a href="/changeaddress">Change of Address</a></li>
-    <li class="collection-item"><a href="/laterent">Late Rent Notice</a></li>
-    <li class="collection-item"><a href="/leaserenewal">Lease Renewal Notice</a></li>
+    <li class="collection-item"><a href="/threedaynotice" class="link">Three-Day Notice (Pre-Eviction)</a></li>
+    <li class="collection-item"><a href="/changeaddress" class="link">Change of Address</a></li>
+    <li class="collection-item"><a href="/laterent" class="link">Late Rent Notice</a></li>
+    <li class="collection-item"><a href="/leaserenewal" class="link">Lease Renewal Notice</a></li>
   </ul>
 </div>
 @endsection
