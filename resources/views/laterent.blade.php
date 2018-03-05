0c9f0218bd0acded8e829b9e3db535d8bce29073
Mark Powalisz <markpowalisz@Fenix-Macbook-Pro.local>
2018-03-05 11:07:46 -0500

    Updating late rent form

--- resources/views/laterent.blade.php
+++ resources/views/laterent.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-{{--  {{$properties->tenant}}<br>  --}}
-{{--  {{$properties->addr}}<br>  --}}
-{{--  {{$properties->addr2}}<br>  --}}
-{{--  {{$properties->city}}{{$properties->state}}{{$properties->zip}}  --}}
+    <div class="form">
+{{$property->tenant}}<br>
+{{$property->addr}}<br>
+{{$property->addr2}}<br>
+{{$property->city}}{{$property->state}}{{$property->zip}}
 <br>
 <p>Dear Tenants,</p>
 <br>
@@ -13,5 +14,6 @@
 <p>As of today, per your lease agreement, you have been assessed a $____ late fee charge. The total amount due as of today is $_______. This includes the $_______ monthly rental and $______ in late fees. If payment is not received Landlord may exercise its rights under the Florida Lease Agreement including eviction proceedings. Should it become necessary for Landlord to employ an attorney to enforce any of the conditions or covenants of the Florida Lease Agreement, including the collection of rentals or gaining possession of the Premises, Tenant will be responsible to pay all expenses so incurred, including reasonable attorneys' fees.</p>
 <br>
 <p>If you have any questions regarding this matter, you can contact me at _________________.  Failure to pay the total amount noted on the above late rent notice, plus the additional late fees as they accrue, may result in further actions being taken, but not limited to eviction.  Please govern yourself accordingly.</p>
+</div>
 </div>
 @endsection
