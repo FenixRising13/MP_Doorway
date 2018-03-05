4059f947d1daba6d38848ceb66848878ab9acc63
Mark Powalisz <markpowalisz@Fenix-Macbook-Pro.local>
2018-03-05 00:10:04 -0500

    Updated Materialize class for forms

--- resources/views/forms.blade.php
+++ resources/views/forms.blade.php
@@ -1,11 +1,11 @@
 @extends ('layouts.app')
 @section ('content')
 <div class="container">
-  <ul>
-    <li>Three-Day Notice (Pre-Eviction)</li>
-    <li>Change of Address</li>
-    <li>Late Rent Notice</li>
-    <li>Lease Renewal Notice</li>
+  <ul class="collection">
+    <li class="collection-item">Three-Day Notice (Pre-Eviction)</li>
+    <li class="collection-item">Change of Address</li>
+    <li class="collection-item">Late Rent Notice</li>
+    <li class="collection-item">Lease Renewal Notice</li>
   </ul>
 </div>
 @endsection
