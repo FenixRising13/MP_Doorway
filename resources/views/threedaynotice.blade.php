e24af851982099ba1542962934982d8325cbe7a9
Mark Powalisz <markpowalisz@Fenix-Macbook-Pro.local>
2018-03-05 11:48:25 -0500

    Corrected three day notice

--- resources/views/threedaynotice.blade.php
+++ resources/views/threedaynotice.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app') 
 @section('content')
-<div class="container-">
+<div class="container">
     <div class="form">
         <h3>Three Day Notice to Vacate</h3>
         <p>Content will go here...</p>
