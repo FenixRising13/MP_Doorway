e9927941bc36ad5a51cedd8710ae7e923411e1cd
Mark Powalisz <markpowalisz@Fenix-Macbook-Pro.local>
2018-03-06 12:04:23 -0500

    Three day notice

--- resources/views/threedaynotice.blade.php
+++ resources/views/threedaynotice.blade.php
@@ -1,11 +1,40 @@
-{{--  Three Day Notice Form  --}}
+{{-- Three Day Notice Form --}} 
 @extends('layouts.app') 
 @section('content')
 <div class="container">
     <div class="form">
-        {{--  Heading  --}}
-        <h5>Three Day Notice to Vacate</h5>
-        <p>Content will go here...</p>
+        {{-- Heading --}}
+        <h5>Three-day Notice to Tenant</h5><br>
+        <h5>Demand for Payment or Possession</h5>
+
+        <b>Florida Statutes 83.56</b>
+        <br>
+        <p>Seminole County, Florida</p> <p>November 13, 2012</p>
+        <br>
+        <p>Tenant</p>
+        <p>Address</p>
+        <p>City State Zip</p>
+        <br>
+        Mr. Garcia, This notice is to inform you that we have not received your rent payment for November 2012, which was
+        due by November 5, 2012 by 5:00PM. This was for rental period of November 1, 2012 to November 30, 2012. You are hereby
+        notified that you are indebted to me, as of today, $1,112.86 for the rent and use of the premises noted above. Per
+        your lease agreement, you have been assessed a $210.00 late fee charge at $30.00 per day and still owe the remaining
+        balance of $102.86 for the outstanding electric bill, which is included in the above amount due. I demand payment
+        of rent in full or possession of the premises within three (3) days (excluding Saturday, Sunday, and legal holidays)
+        from the date of delivery of this notice. Please submit the total amount due to this address: Larry Powalisz 1375
+        State Road 436 Casselberry, FL 32707 321-231-2843 Failure to pay the total amount noted on the above late rent notice
+        may result in further actions being taken, but not limited to eviction. Sincerely, Larry Powalisz, Landlord I hereby
+        certify that a copy of the following Notice was served upon the above named tenant(s) at the above address on the
+        following date: (month/day/year) ________________________________ by the following means:
+        <br>
+        <p>( ) Certified Mail - Return Receipt Requested</p>
+        <p>( ) Hand delivered to tenant</p>
+        <p>( ) Leaving a copy at the premises</p>
+        <br>
+       
+        <br>
+        ___________________________
+        Signature
     </div>
 </div>
 @endsection
