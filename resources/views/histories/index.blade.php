b14cd5cec62b226b37e8a90dbda35b4c84a85c68
Mark Powalisz <markpowalisz@Fenix-Macbook-Pro.local>
2018-03-06 11:31:11 -0500

    Updates histories@index again

--- resources/views/histories/index.blade.php
+++ resources/views/histories/index.blade.php
@@ -4,20 +4,10 @@
     {{-- Header --}}
     <h3>History</h3>
     <hr> @foreach ($histories as $history)
-    <ul class='collection'>
-        <li class='collection-item'>
-            <b>Property:</b>{{$history->property_id}}
-        </li>
-        <li class='collection-item'>
-            <b>Description:</b>{{$history->description}}
-        </li>
-        <li class='collection-item'>
-            <b>Rent:</b>{{$history->rent}}
-        </li>
-        <li class='collection-item'>
-            <b>Date Received:</b>{{$history->received}}
-        </li>
-    </ul>
-    @endforeach
+    <b>Property: </b>{{$history->property_id}}
+    <b>Description: </b>{{$history->description}}
+    <b>Rent: </b>{{$history->rent}}
+    <b>Date Received: </b>{{$history->received}} @endforeach
+    <button class="waves-effect waves-light grey btn">Enter Rent</button>
 </div>
 @endsection
