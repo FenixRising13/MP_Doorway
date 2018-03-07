1d42cc3b3771149822050ebe13c3b1f0885e7d61
Mark Powalisz <markpowalisz@Fenix-Macbook-Pro.local>
2018-03-07 16:09:30 -0500

    Updated Properties@create

--- resources/views/properties/create.blade.php
+++ resources/views/properties/create.blade.php
@@ -1,7 +1,40 @@
-{{--  Properties@Create  --}}
+{{-- Properties@Create --}} 
 @extends('layouts.app') 
 @section('content')
-    <div class="container">
-       <p>This is where the create form will go...</p>
-    </div>
+<div class="container">
+    <form action="/properties" method="POST">
+        {{ csrf_field() }} {{ method_field('POST') }}
+        <div class="input-field col s12 l6">
+            <input type="text" id="addr" class="validate" placeholder="Address">
+            <label for="addr"></label>
+        </div>
+        <div class="input-field col s12 l6">
+            <input type="text" id="addr2" class="validate" placeholder="Address 2">
+            <label for="addr2"></label>
+        </div>
+        <div class="input-field col s12 l6">
+            <input type="text" id="city" class="validate" placeholder="City">
+            <label for="city"></label>
+        </div>
+        <div class="input-field col s12 l6">
+            <input type="text" id="state" class="validate" placeholder="State">
+            <label for="state"></label>
+        </div>
+        <div class="input-field col s12 l6">
+            <input type="number" id="zip" class="validate" placeholder="Zip">
+            <label for="zip"></label>
+        </div>
+        <div class="input-field col s12 l6">
+            <input type="text" id="community" class="validate" placeholder="Community">
+            <label for="community"></label>
+        </div>
+        <div class="input-field col s12 l6">
+            <input type="date" id="lease_date" class="validate" placeholder="Lease Date">
+            <label for="lease_date"></label>
+        </div>
+        <button class="btn grey" id="submit">
+            Submit
+        </button>
+    </form>
+</div>
 @endsection
